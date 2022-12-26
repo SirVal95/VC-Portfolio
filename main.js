f6e7a2cc7f9b1ff0cd77da2a428b7f76c799448c
unknown <valentinechima95@gmail.com>
2022-12-26 19:25:09 +0100

    projects

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 ScrollReveal({
     reset: true,
-    distance:'60px',
+    distance:'80px',
     delay: 50,
     duration: 1500
 });
 
 ScrollReveal().reveal('.navbar h2, .about-me-left', { delay: 50, interval:100, origin: 'left' });
 ScrollReveal().reveal('.navbar .social-icons', { delay: 50, origin: 'right' });
-ScrollReveal().reveal('.services h1', { delay: 50, origin: 'top' });
-ScrollReveal().reveal('.about-me img, .service-box', { delay: 50, origin: 'bottom', interval:100});
+ScrollReveal().reveal('.services h1, .header-1-and-link h1', { delay: 50, origin: 'top' });
+ScrollReveal().reveal('.about-me img, .service-box, .project-box, .header-1-and-link a', { delay: 50, origin: 'bottom', interval:100});
