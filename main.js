52c933ab6a82446820cc2244b1de2d6808db9fc1
unknown <valentinechima95@gmail.com>
2022-12-27 11:10:36 +0100

    handburger menu

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const menu_btn = document.querySelector(".hamburger-menu");
+const mobile_menu = document.querySelector(".mobile-nav");
+
+menu_btn.onclick = function(){
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+}
+
 ScrollReveal({
     reset: true,
     distance:'60px',
